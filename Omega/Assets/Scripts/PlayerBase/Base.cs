0c40eb7f59f770df13066ade37f138f606b44965
EUANS-PC\Euan Jones <s213321@students.aie.edu.au>
2023-05-03 22:37:55 +0930

    Updated PlayerBase Scriptable Object, Created 6 varients

--- Omega/Assets/Scripts/PlayerBase/Base.cs
+++ Omega/Assets/Scripts/PlayerBase/Base.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Omega.Core
 {
     [CreateAssetMenu(fileName = "New BaseVarient", menuName = "Create New PlayerBase")]
     public class Base : ScriptableObject
     {
-        [SerializeField] GameObject BaseVarientPrefab;
-        [SerializeField] GameObject TurnOrderVarientPrefab;
+        [SerializeField] public string factionName;
+        [SerializeField] List<GameObject> BaseVarientPrefabList;
+        [SerializeField] public Material materialvarientOverrite;
+        [SerializeField] public Image turnOrderVarientIcon;
+
+        public GameObject GetBaseVarientPrefab()
+        {
+            return BaseVarientPrefabList[Random.Range(0, BaseVarientPrefabList.Count)];
+        }
 
     }
 }
