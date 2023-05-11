4583f60876cdfb2e8ec95fdeef29b2abf3b70213
EUANS-PC\Euan Jones <s213321@students.aie.edu.au>
2023-05-11 17:57:48 +0930

    Added camera shake script (not finished)

--- Omega/Assets/CameraShake.cs
+++ Omega/Assets/CameraShake.cs
@@ -1,18 +1,45 @@
+using Cinemachine;
+using Omega.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour
+namespace Omega.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraShake : MonoBehaviour
     {
-        
-    }
+        public float shakeTimer = 10;
+        public float intensity = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        CinemachineVirtualCamera cinemachineVirtualCamera;
+        PlayerIdentifier playerIdentifier;
+
+        private void Awake()
+        {
+            playerIdentifier = FindObjectOfType<PlayerIdentifier>();
+        }
+        private void UpdateCamera()
+        {
+            cinemachineVirtualCamera = playerIdentifier.currentPlayer.GetComponent<CinemachineVirtualCamera>();
+        }
+
+        private void Update()
+        {
+            if(shakeTimer > 0)
+            {
+                shakeTimer -= Time.deltaTime;
+                if(shakeTimer <= 0f)
+                {
+                    CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                    cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+                }
+            }
+        }
+        private void ShakeCamera(float intensity, float time)
+        {
+            CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+            shakeTimer = time;
+        }
     }
 }
