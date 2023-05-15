e3a5aa3f3279cbcf2494a6e46ccac9dff63f22bc
AIE\s213321 <s213321@students.aie.edu.au>
2023-05-15 10:17:03 +0930

    Added rolling numbers prefabs, and basic scripts to parents and children

---
+++ Omega/Assets/NumbersCollection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Omega.Actions
+{
+    public class NumbersCollection : MonoBehaviour
+    {
+        [SerializeField] public List<GameObject> numbersList = new List<GameObject>();
+    }
+}
