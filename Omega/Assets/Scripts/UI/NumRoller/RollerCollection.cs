944c5b1b07b166cc7855988143d6a90f5bbdf868
JacksonH <jacksonhole24@gmail.com>
2023-06-05 20:31:39 +0930

    Changed Number Roller

--- Omega/Assets/Scripts/UI/NumRoller/RollerCollection.cs
+++ Omega/Assets/Scripts/UI/NumRoller/RollerCollection.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Omega.Actions
 {
     public class RollerCollection : MonoBehaviour
     {
-        [SerializeField] public GameObject hundredsRoller = null;
-        [SerializeField] public GameObject tensRoller = null;
-        [SerializeField] public GameObject unitsRoller = null;
+        [SerializeField] public TextMeshProUGUI hundredsRoller = null;
+        [SerializeField] public TextMeshProUGUI tensRoller = null;
+        [SerializeField] public TextMeshProUGUI unitsRoller = null;
     }
 }
